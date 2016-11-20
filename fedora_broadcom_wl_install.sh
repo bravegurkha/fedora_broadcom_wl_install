e0b9fe37ad894d82a7788a6cc2c2a490ffe60f70
Swornim Shrestha <srestaswrnm@gmail.com>
2016-11-20 15:30:08 +0545

    Update fedora_broadcom_wl_install.sh

--- fedora_broadcom_wl_install.sh
+++ fedora_broadcom_wl_install.sh
@@ -1,46 +1,21 @@
-#/usr/bin/env bash
+#! /bin/bash
 
-# Install some pacakages we'll need to compile the driver below.
-sudo dnf install gcc kernel-devel -y
+#Set Version for Fedora
+FedoraVers=$(rpm -E %fedora)
 
-# Create working dir for Broadcom driver files and patches.
-mkdir hybrid_wl_f24
+#Download and install RPM fusion repo
+wget http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$FedoraVers.noarch.rpm
+dnf install rpmfusion-nonfree-release-$FedoraVers.noarch.rpm -y
 
-# Change to working dir.
-cd hybrid_wl_f24
+#Clean yum metadata install adkmod-wl and kernel-devel
+dnf clean all -y
+dnf install akmod-wl -y 
+dnf install kernel-devel -y
 
-if [ 'x86_64' == `uname -m` ]; then
-	# 64-bit driver files.
-	FILE='hybrid-v35_64-nodebug-pcoem-6_30_223_271.tar.gz'
-else
-	# 32-bit driver files.
-	FILE='hybrid-v35-nodebug-pcoem-6_30_223_271.tar.gz'
-fi
+#Force Broadcom driver and modprobing kernel
+akmods --force
+modprobe wl
 
-# Download Broadcom Linux Wi-Fi driver.
-wget http://www.broadcom.com/docs/linux_sta/$FILE
-
-# Extract driver files.
-tar zxvf $FILE
-
-# Compile driver.
-make clean && make
-
-# Install driver.
-sudo make install
-
-# Update available drivers.
-sudo depmod -a
-
-# Unload conflicting drivers.
-sudo rmmod b43 ssb bcma
-
-# Load the driver.
-sudo modprobe wl
-
-# Blacklist conflicting drivers.
-printf 'blacklist b43\nblacklist ssb\nblacklist bcma\n' | sudo tee /etc/modprobe.d/wl.conf
-
-# Load driver automatically at boot time.
-echo 'wl' | sudo tee /etc/modules-load.d/wl.conf
-
+#runs iwconfig (wireless equivalent of ifconfig) and restarts NetworkManager
+iwconfig
+systemctl restart NetworkManager
